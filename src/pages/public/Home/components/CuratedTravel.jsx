97284e289bc30c95e9fe6bb1606e742b683b99f0
shanjid5566 <shanjidahmed66@gmail.com>
2026-06-13 12:18:32 +0600

    feat: add CuratedTravel component and define global theme variables in index.css

--- src/pages/public/Home/components/CuratedTravel.jsx
+++ src/pages/public/Home/components/CuratedTravel.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const CuratedTravel = () => {
   return (
-    <section className="w-full bg-[#FFFFFF] font-sans px-6 pt-16 md:px-16 md:pt-24 selection:bg-blue-100">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-8 items-center">
-        
+    <section className="w-full bg-[#FAFAFA] font-sans selection:bg-blue-100">
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-12 gap-12 lg:gap-8 items-center px-4 py-8 my-8 md:px-6 md:py-12 md:mt-12">
         {/* Left Column: Content (5 Cols) */}
-        <div className="lg:col-span-5 flex flex-col items-start space-y-6 max-w-md lg:max-w-full">
-          <h2 className="text-[#0F172A] text-3xl md:text-4xl lg:text-5xl font-serif font-semibold tracking-tight leading-tight">
+        <div className="lg:col-span-6 flex flex-col items-start space-y-6 max-w-md lg:max-w-full">
+          <h2 className="text-[#0F172A] text-3xl md:text-4xl  font-serif font-semibold tracking-tight leading-tight">
             Curated Travel
           </h2>
-          <p className="text-[#475569] text-[15px] md:text-[16px] leading-relaxed font-normal">
-            Do you travel between NYC and Tampa? Join other members and experience the luxury of a shared private jet.
+          <p className="text-[#475569] text-sm md:text-base leading-relaxed font-semibold">
+            Do you travel between NYC and Tampa? Join other <br/> members and
+            experience the luxury of a shared private jet.
           </p>
           {/* Custom Pill-shaped Button */}
           <button className="bg-[#2563EB] hover:bg-[#1D4ED8] text-white font-medium text-[14px] px-7 py-3 rounded-full transition-colors duration-200 shadow-sm hover:shadow">
@@ -19,9 +19,14 @@ const CuratedTravel = () => {
           </button>
         </div>
 
-        {/* Right Column: Image Grid Gallery (7 Cols) */}
-      
-
+        {/* Right Column: Image */}
+        <div className="lg:col-span-6 w-full h-[350px] lg:h-[500px] rounded-2xl overflow-hidden">
+          <img
+            src="/Curated_Travel.png"
+            alt="Private jet"
+            className="w-full h-full object-container object-center"
+          />
+        </div>
       </div>
     </section>
   );
