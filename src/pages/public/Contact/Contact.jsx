12b7e3668f57070a14b543ee1b1b99bcb21e61dc
shanjid5566 <shanjidahmed66@gmail.com>
2026-06-13 16:06:16 +0600

    feat: implement Contact page and add FAQ hero banner with assets

--- src/pages/public/Contact/Contact.jsx
+++ src/pages/public/Contact/Contact.jsx
@@ -5,9 +5,9 @@ import GetInTouchHero from "./components/GetInTouchHero";
 export default function Contact() {
   return (
     <section className="mx-auto container px-4 py-12 sm:px-6">
-      <GetInTouchHero/>
-     <ContactMethods/>
-     <ContactForm/>
+      <GetInTouchHero />
+      <ContactMethods />
+      <ContactForm />
     </section>
-  )
+  );
 }
