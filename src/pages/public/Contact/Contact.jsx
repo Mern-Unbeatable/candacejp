593ba8fd1254d03a73672bb4bae5d0dc7b310975
shanjid5566 <shanjidahmed66@gmail.com>
2026-06-13 16:08:00 +0600

    feat: create contact page layout with hero, methods, and form components

--- src/pages/public/Contact/Contact.jsx
+++ src/pages/public/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import GetInTouchHero from "./components/GetInTouchHero";
 
 export default function Contact() {
   return (
-    <section className="mx-auto container px-4 py-12 sm:px-6">
+    <section className="">
       <GetInTouchHero />
       <ContactMethods />
       <ContactForm />
