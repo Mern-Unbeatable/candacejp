6007f8c9f1e23aba2c13cd0933aa8b223fd19cb0
shanjid5566 <shanjidahmed66@gmail.com>
2026-06-13 15:42:58 +0600

    feat: create PublicLayout component with navbar, outlet, and footer structure

--- src/components/layout/public/PublicLayout.jsx
+++ src/components/layout/public/PublicLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Footer from "./footer";
 import ExperienceBanner from "./ExperienceBanner";
 import Navbar from "./navbar";
@@ -6,6 +6,7 @@ import Navbar from "./navbar";
 export default function PublicLayout() {
   return (
     <div className="flex min-h-screen flex-col  text-ink-500">
+      <ScrollRestoration />
       <Navbar />
 
       <main className="flex-1">
